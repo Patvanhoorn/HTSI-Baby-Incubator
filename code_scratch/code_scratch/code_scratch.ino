20ab626fdbd70ee9ea7d95a64d3ea7d73d3fcec3
Vito Cipponeri <vc419@cam.ac.uk>
2024-07-03 14:26:47 +0800

    Sesnor changes to DHT22 and improved ticker

--- code_scratch/code_scratch/code_scratch.ino
+++ code_scratch/code_scratch/code_scratch.ino
@@ -1,13 +1,15 @@
-#include <DHT11.h>
-#include <Ticker.h>
 #include "definitions.h"
+#include <Ticker.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 //#include <PID_v2.h>
-
 //This is going to git!!!
 
-DHT11 dht11(12); //DHT pin 12
 //PID_v2 myPID(Kp, Ki, Kd, PID::Direct);// For PID
-Ticker printSensors(sensor_read,1); //Ticker
+
+Ticker sensorRead(readSensor, 2000);
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,24 +21,27 @@ pinMode(thermoregulation, OUTPUT); //Define all pins
 pinMode(humidifier, OUTPUT);
 pinMode(moisture_sensor, INPUT);
 
+dht.begin();
+sensor_t sensor;
+dht.temperature().getSensor(&sensor);
+
 
 TCCR2B = (TCCR2B & B11111000) | 0x03;    // pin 3 PWM frequency of 928.5 Hz -- brackets added to exclude pin 11
 Time = millis();
 
-int temp = dht11.readTemperature();  //Get an initial value
-int humi = dht11.readHumidity();
-
-printSensors.start(); //Start the ticker for sensor printing
+sensorRead.start(); //Start the ticker for sensor printing
 
 Serial.begin(9600);
 Serial.println("Setting Up");
+
+delay(2000);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Add a time since last reading 
   
-  printSensors.update(); //Update ticker
+  sensorRead.update(); //Update ticker
 
 }
 
