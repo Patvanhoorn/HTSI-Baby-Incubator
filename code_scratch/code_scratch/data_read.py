82727efe7d8f9750562889c563fe6dd9b98eab39
Ashna Jain <ashnajainguelph@gmail.com>
2024-07-05 00:54:06 +0800

    stuff

--- code_scratch/code_scratch/data_read.py
+++ code_scratch/code_scratch/data_read.py
@@ -3,7 +3,7 @@ import time
 import serial
 
 #Intialising the connection to the serial port
-port_name = 'com5'
+port_name = 'com3'
 baud_rate = 9600
 try :
     arduinoData = serial.Serial(port_name,baud_rate)
@@ -20,17 +20,11 @@ arduinoData.flush()
 
 
 #functions
-def acceleration_list(): #Retrieving IMU acceleromter values
-    dataPacket= arduinoData.readline()
+def readData():
+    dataPacket = arduinoData.readline()
     dataPacket = str(dataPacket,'utf-8')
     dataPacket = dataPacket.strip('\r\n')
     datapacket_list  = dataPacket.split()
-    a_n = np.float64(datapacket_list)
-    return(a_n) #returns it as a list of lenght 3 
-
-def readData():
-    dataPacket = arduinoData.readline()
-
     print(dataPacket)
 
 while True:
