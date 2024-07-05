d31a2e32f0adcdb32d442f2746e720c1e2a0a7f5
Vito Cipponeri <vc419@cam.ac.uk>
2024-07-05 15:24:15 +0800

    sending code over

--- code_scratch/code_scratch/data_read.py
+++ code_scratch/code_scratch/data_read.py
@@ -4,7 +4,7 @@ import serial
 import numpy as np
 
 #Intialising the connection to the serial port
-port_name = 'com3'
+port_name = 'com5'
 baud_rate = 9600
 try :
     arduinoData = serial.Serial(port_name,baud_rate)
@@ -18,3 +18,13 @@ except serial.SerialException:
 arduinoData = serial.Serial(port_name,baud_rate)
 time.sleep(2)
 arduinoData.flush()
+
+
+#functions
+def acceleration_list(): #Retrieving IMU acceleromter values
+    dataPacket= arduinoData.readline()
+    dataPacket = str(dataPacket,'utf-8')
+    dataPacket = dataPacket.strip('\r\n')
+    datapacket_list  = dataPacket.split()
+    a_n = np.float64(datapacket_list)
+    return(a_n) #returns it as a list of lenght 3 
